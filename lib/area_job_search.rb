a0a582fc83c913fdf98e3486c416b56ed481b4e4
Steven Laff <steve.laff@gmail.com>
2019-10-10 17:51:16 +0000

    updated environment requirements

--- lib/area_job_search.rb
+++ lib/area_job_search.rb
@@ -1,9 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require "area_job_search/version"
-
-module AreaJobSearch
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
 require_relative './area_job_search/job'
 require_relative './area_job_search/cli'
