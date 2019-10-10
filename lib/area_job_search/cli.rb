abcd434d124eddc729f46c14319f5297f53b6259
Steven Laff <50171851+LaffSteven@users.noreply.github.com>
2019-10-10 16:22:46 -0400

    continued building cli methods

--- lib/area_job_search/cli.rb
+++ lib/area_job_search/cli.rb
@@ -1,16 +1,61 @@
 class AreaJobSearch::CLI
 
-  # Testing a load error
-  # Remove this after testing
-  #def initialize
-  #  puts "Created new CLI"
-  #  puts "Test Successful!"
-  #end
-
   def call
-    puts "Welcome to the Area Job Search Assistant"
+    poll_user
+    list_jobs
+    menu
+    goobye
   end
 
 ########    Methods Below Here    ########
 
+  def poll_user
+    puts "Welcome to the Area Job Search Assistant"
+    get_zip
+    get_miles
+    confirm_search
+  end
+  
+  def get_zip
+    puts "Please enter the zip code you would like to search"
+    @zip = gets.strip
+    sleep(0.25)
+  end
+  
+  def get_miles
+    puts "Please enter the distance from #{@zip} you want to search:"
+    @miles = gets.strip
+    sleep(0.25)
+  end
+  
+
+  def confirm_search
+    puts "You want to search for Jobs within #{@miles} miles of #{@zip}?"
+    puts "Please yes or no"
+    input = nil
+    input = gets.strip.downcase
+    yes_or_no(input)
+  end
+   
+  def yes_or_no(i)
+    case i
+      when "yes" || "y"
+        search_confirmed
+      when "no" || "n"
+        early_goodbye
+      else
+        "Error: invalid input ${i}"
+        early_goodbye
+    end
+  end  
+  
+  def search_confirmed
+    puts "You are now searching for Software Jobs within #{@miles} miles of #{@zip}"
+  end
+  
+  def early_goodbye
+    puts "Sorry, please start over"
+    exit!
+  end
+  
 end
