e093f6683a22bdb3bce3a8ce8ec9a8962e7cf0c8
Steven Laff <steve.laff@gmail.com>
2019-10-10 18:13:41 +0000

    working on dependencies

--- lib/area_job_search.rb
+++ lib/area_job_search.rb
@@ -2,6 +2,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require "area_job_search/version"
-require_relative './area_job_search/job'
-require_relative './area_job_search/cli'
+require "./area_job_search/version"
+require "./area_job_search/job"
+require "./area_job_search/cli"
